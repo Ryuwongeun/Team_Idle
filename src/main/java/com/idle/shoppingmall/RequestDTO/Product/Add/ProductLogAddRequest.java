d798b306bd88a61d3390536bf10152828e0042c8
Leeseunghoon <dl11911@naver.com>
2024-04-04 17:44:38 +0900

    dd

--- src/main/java/com/idle/shoppingmall/RequestDTO/Product/Add/ProductLogAddRequest.java
+++ src/main/java/com/idle/shoppingmall/RequestDTO/Product/Add/ProductLogAddRequest.java
@@ -1,8 +1,4 @@
-<<<<<<< HEAD:src/main/java/com/idle/shoppingmall/RequestDTO/CartAddRequest.java
-package com.idle.shoppingmall.RequestDTO;
-=======
 package com.idle.shoppingmall.RequestDTO.Product.Add;
->>>>>>> 3f46679d864c1e733ea0ab9d03d6c945baeab7b7:src/main/java/com/idle/shoppingmall/RequestDTO/Product/Add/ProductLogAddRequest.java
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
