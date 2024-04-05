53e48c1db7cc4fe48fb5d9ad6097baced800b72e
Kim-soung-won <rlatmddnjs0103@naver.com>
2024-04-05 23:18:09 +0900

    승훈씨가 만든 페이징 페이지에 연동

--- src/main/java/com/idle/shoppingmall/ControllerView/MainViewController.java
+++ src/main/java/com/idle/shoppingmall/ControllerView/MainViewController.java
@@ -30,5 +30,10 @@ public class MainViewController {
         return "/FE/join";
     } // join
 
+    @GetMapping("/FE/new")
+    String NEW() {
+        return "/FE/new";
+    } // join
+
 
 } // end class
---
+++ src/main/resources/static/JS/productCardList.js
@@ -0,0 +1,39 @@
+window.onload = () => {
+    GetListRequest(`/api/GET/productList`);
+}
+
+function GetListRequest(url){
+    const headers = {
+        'Content-Type': 'application/json',
+    };
+    fetch(url, {
+        method: 'POST',
+        headers: headers
+    })
+        .then(response => response.json())
+        .then(data => {
+            // Thymeleaf로 직접 데이터 추가
+            const productDataContainer = document.getElementById('field');
+            // 데이터 배열을 순회하면서 각 항목을 HTML로 변환
+            let productsHtml = data.map(item => {
+                return `
+                <article class="bg-white shadow-md rounded overflow-hidden">
+                    <img src="https://source.unsplash.com/random/300x300?clothes&sig=${item.id}"
+                         alt="Fashion item" class="w-full h-64 object-cover"
+                         onclick=redirectTo(${item.product_id})>
+                    <div class="p-4">
+                        <h3 class="font-semibold">상품명 : ${item.pd_name}</h3>
+                        <p class="text-gray-600">${item.pd_price}원</p>
+                        <p class="text-gray-600">화이팅^^</p>
+                        <p class="text-gray-600">${item.status}</p>
+                        <p class="text-gray-600">${item.msg}</p>
+                    </div>
+                </article>
+            `;
+            }).join(''); // 배열의 모든 항목을 하나의 문자열로 결합
+            productDataContainer.innerHTML = productsHtml;
+        })
+        .catch(error => {
+            console.error('Error fetching user data:', error);
+        });
+}
