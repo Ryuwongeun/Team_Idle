9a2700ab69a9f2b043877e18137a5d91ee581fce
Kim-soung-won <rlatmddnjs0103@naver.com>
2024-04-08 10:08:48 +0900

    페이지에서 이메일 인증 체크 추가

--- src/main/java/com/idle/shoppingmall/Config/Mail/AccountController.java
+++ src/main/java/com/idle/shoppingmall/Config/Mail/AccountController.java
@@ -1,6 +1,8 @@
 package com.idle.shoppingmall.Config.Mail;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -8,10 +10,13 @@ import org.springframework.web.bind.annotation.*;
 public class AccountController {
     private final RegisterMail registerMail;
     @PostMapping("/login/mailConfirm")
-    public String mailConfirm(@RequestBody EmailRequestDTO request) throws Exception {
+    public ResponseEntity<EmailResponseDTO> mailConfirm(@RequestBody @Valid EmailRequestDTO request) throws Exception {
         System.out.println("이메일 : " + request.email);
-        String code = registerMail.sendEmail(request.email);
-        System.out.println("인증코드 : " + code);
-        return code;
+        String data = registerMail.sendEmail(request.email);
+        System.out.println("인증코드 : " + data);
+        if(data != null) {
+            return ResponseEntity.ok().body(new EmailResponseDTO(200, "인증 메일을 전송했습니다.", data));
+        }
+        return ResponseEntity.ok().body(new EmailResponseDTO(400, "메일 전송에 실패했습니다.", null));
     }
 }
