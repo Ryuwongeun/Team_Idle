4882246ad409418e2a77eb20a7b3220df867a367
SOOYEON21428 <joenpark06@naver.com>
2024-04-16 11:30:06 +0900

    pull

--- src/main/resources/static/JS/WongeunJS/like.js
+++ src/main/resources/static/JS/WongeunJS/like.js
@@ -16,15 +16,13 @@ function GetListRequest(url){
     })
         .then(response => response.json())
         .then(data => {
-            // Thymeleaf로 직접 데이터 추가
             const LatestViewController = document.getElementById('field');
-            // 데이터 배열을 순회하면서 각 항목을 HTML로 변환
             let productsHtml = data.map(item => {
                 return `
-                <article class="bg-white shadow-md rounded overflow-hidden">
+                <article class="bg-white shadow-md rounded overflow-hidden product-item">
                     <img src="https://source.unsplash.com/random/300x300?clothes&sig=${item.product_id}"
                          alt="Fashion item" class="w-full h-64 object-cover"
-                         onclick=redirectTo(${item.product_id})>
+                         onclick="redirectTo(${item.product_id})">
                     <div class="p-4">
                         <h3 class="font-semibold">브랜드명 : ${item.product_id}</h3>
                         <h3 class="font-semibold">상품명 : ${item.pd_name}</h3>
@@ -34,11 +32,10 @@ function GetListRequest(url){
                     </div>
                 </article>
             `;
-            }).join(''); // 배열의 모든 항목을 하나의 문자열로 결합
+            }).join('');
             LatestViewController.innerHTML = productsHtml;
         })
         .catch(error => {
-                console.error('Error fetching user data:', error);
-            }
-        );
+            console.error('Error fetching user data:', error);
+        });
 }
