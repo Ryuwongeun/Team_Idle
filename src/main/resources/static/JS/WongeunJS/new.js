ee30ecdc7bc901fe92708b4ad881045d71070364
SOOYEON21428 <joenpark06@naver.com>
2024-04-17 16:01:02 +0900

    0417_2

--- src/main/resources/static/JS/WongeunJS/new.js
+++ src/main/resources/static/JS/WongeunJS/new.js
@@ -2,26 +2,21 @@ window.onload = () => {
     GetListRequest(`/view/GET/Latest?page=1`);
 }
 
-let page = 1;//현재 페이지 번호
+let page = 1; // 현재 페이지 번호
 
-function redirectTo(id){
+function redirectTo(id) {
     location.href = `/product/?id=${id}`;
 }
 
-//스크롤 이벤트 리스너 추가
-window.onscroll=()=>{
-    //사용자가 페이지 하단에 도달했는지 확인
-    if(window.innerHeight + window.scrollY >=document.body.offsetHeight){
-        GetListRequest(`/view/GET/Latest?page=${++page}`);//다음 페이지 데이터 로드
+// 스크롤 이벤트 리스너 추가
+window.onscroll = () => {
+    // 사용자가 페이지 하단에 도달했는지 확인
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+        GetListRequest(`/view/GET/Latest?page=${++page}`); // 다음 페이지 데이터 로드
     }
 };
 
-<<<<<<< HEAD
-function GetListRequestByCreatedAt(url){
-=======
-function GetListRequest(url){
-
->>>>>>> 68175710f5e620687979c6e2196baf5cde7e4645
+function GetListRequest(url) {
     const headers = {
         'Content-Type': 'application/json',
     };
@@ -32,27 +27,24 @@ function GetListRequest(url){
     })
         .then(response => response.json())
         .then(data => {
-
-
             // 데이터 배열을 순회하면서 각 항목을 HTML로 변환
-
             const LatestViewController = document.getElementById('field');
 
             let productsHtml = data.map(item => {
                 return `
-                        <article class="product-item bg-white shadow-md rounded overflow-hidden">
-                            <img src="https://source.unsplash.com/random/300x300?clothes&sig=${item.product_id}"
-                                 alt="Fashion item"
-                                 onclick="redirectTo(${item.product_id})"
-                                 onmouseover="scaleImage(this, 1.2)" 
-                                 onmouseout="scaleImage(this, 1)">
-                            <div class="p-4">
-                                 <h3 class="font-semibold">브랜드명 : ${item.product_id}</h3>
-                                <h3 class="font-semibold">상품명 : ${item.pd_name}</h3>
-                                <p class="text-gray-600">${item.pd_price}원</p>                 
-                                <p class="text-gray-600" >${item.product_id}</p>
-                            </div>
-                        </article>`
+                <article class="product-item bg-white shadow-md rounded overflow-hidden">
+                    <img src="https://source.unsplash.com/random/300x300?clothes&sig=${item.product_id}"
+                         alt="Fashion item"
+                         onclick="redirectTo(${item.product_id})"
+                         onmouseover="scaleImage(this, 1.2)" 
+                         onmouseout="scaleImage(this, 1)">
+                    <div class="p-4">
+                        <h3 class="font-semibold">브랜드명 : ${item.product_id}</h3>
+                        <h3 class="font-semibold">상품명 : ${item.pd_name}</h3>
+                        <p class="text-gray-600">${item.pd_price}원</p>                 
+                        <p class="text-gray-600" >${item.product_id}</p>
+                    </div>
+                </article>`;
             }).join('');
             LatestViewController.innerHTML += productsHtml;
         })
