ab3db0bea879a491fda80fd809b1507f549f6723
Kim-soung-won <rlatmddnjs0103@naver.com>
2024-04-17 17:14:45 +0900

    push

--- src/main/java/com/idle/shoppingmall/Controller/ControllerView/ProductViews/DTO/PaymentListDTOtoSession.java
+++ src/main/java/com/idle/shoppingmall/Controller/ControllerView/ProductViews/DTO/PaymentListDTOtoSession.java
@@ -9,6 +9,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class PaymentListResponse {
+    private Long id;
     private String name;
     private String size;
 }
---
+++ src/main/java/com/idle/shoppingmall/Controller/ControllerView/ProductViews/DTO/PaymentListViewDTO.java
@@ -0,0 +1,2 @@
+package com.idle.shoppingmall.Controller.ControllerView.ProductViews.DTO;public class PaymentListViewDTO {
+}
