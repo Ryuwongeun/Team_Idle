e9467be20ab43fcc3856de2e5c9d203b7b76eab0
Ryuwongeun <wongeun0311@gmail.com>
2024-04-18 09:32:49 +0900

    category

--- src/main/resources/static/JS/WongeunJS/new.js
+++ src/main/resources/static/JS/WongeunJS/new.js
@@ -1,48 +1,44 @@
-// let timeout;
-//
-// window.onload = () => {
-//     GetListRequestLatest(`/view/GET/Latest?page=1`);
-// }
-//
-// let page = 1; // 현재 페이지 번호
-//
-// function redirectTo(id) {
-//     location.href = `/product/?id=${id}`;
-// }
-//
-// window.addEventListener('scroll', function() {
-//     // 이전에 설정된 타임아웃이 있다면 취소
-//     if (timeout) {
-//         clearTimeout(timeout);
+let timeout;
+
+window.onload = () => {
+    GetListRequestLatest(`/view/GET/Latest?page=1`);
+}
+
+let page = 1; // 현재 페이지 번호
+
+function redirectTo(id) {
+    location.href = `/product/?id=${id}`;
+}
+
+window.addEventListener('scroll', function() {
+    // 이전에 설정된 타임아웃이 있다면 취소
+    if (timeout) {
+        clearTimeout(timeout);
+    }
+
+    timeout = setTimeout(function() {
+        let documentHeight = document.body.scrollHeight;
+        let viewportHeight = window.innerHeight;
+        let currentScroll = window.scrollY;
+        let triggerPoint = documentHeight - viewportHeight - 10;
+
+        if (currentScroll >= triggerPoint) {
+            GetListRequestLatest(`/view/GET/Latest?page=${++page}`);//다음 페이지 데이터 로드
+        }
+        console.log(page);
+    },1000);
+});
+
+//스크롤 이벤트 리스너 추가
+// window.onscroll=()=>{
+//     //사용자가 페이지 하단에 도달했는지 확인
+//     if(window.innerHeight + window.scrollY >=document.body.offsetHeight){
+//         GetListRequestLatest(`/view/GET/Latest?page=${++page}`);//다음 페이지 데이터 로드
 //     }
-//
-// <<<<<<< HEAD
-// function GetListRequest(url){
-// =======
-//     timeout = setTimeout(function() {
-//         let documentHeight = document.body.scrollHeight;
-//         let viewportHeight = window.innerHeight;
-//         let currentScroll = window.scrollY;
-//         let triggerPoint = documentHeight - viewportHeight - 10;
-//
-//         if (currentScroll >= triggerPoint) {
-//             GetListRequestLatest(`/view/GET/Latest?page=${++page}`);//다음 페이지 데이터 로드
-//         }
-//         console.log(page);
-//     },1000);
-// });
-//
-// //스크롤 이벤트 리스너 추가
-// // window.onscroll=()=>{
-// //     //사용자가 페이지 하단에 도달했는지 확인
-// //     if(window.innerHeight + window.scrollY >=document.body.offsetHeight){
-// //         GetListRequestLatest(`/view/GET/Latest?page=${++page}`);//다음 페이지 데이터 로드
-// //     }
-// //     console.log(page)
-// // };
-//
-// function GetListRequestLatest(url){
-// >>>>>>> 0f2323219d282fd9727c63005a6260b308ac1b44
+//     console.log(page)
+// };
+
+function GetListRequestLatest(url){
 
     const headers = {
         'Content-Type': 'application/json',
